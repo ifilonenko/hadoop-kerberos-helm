d4e689701531c9eef5f640ce370e1e74440f6fa2
foxish <ramanathana@google.com>
2017-07-11 02:46:14 -0700

    Final changes for minikube

--- start-kdc.sh
+++ start-kdc.sh
@@ -7,24 +7,23 @@
 /usr/sbin/kadmin.local -q "addprinc  -randkey ifilonenko"
 /usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/ifilonenko.keytab ifilonenko"
 
-/usr/sbin/kadmin.local -q "addprinc -randkey HTTP/server"
-/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/server.keytab HTTP/server"
+/usr/sbin/kadmin.local -q "addprinc -randkey HTTP/server.default.svc.cluster.local"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/server.keytab HTTP/server.default.svc.cluster.local"
 
-/usr/sbin/kadmin.local -q "addprinc -randkey hdfs/nn"
-/usr/sbin/kadmin.local -q "addprinc -randkey HTTP/nn"
-/usr/sbin/kadmin.local -q "addprinc -randkey hdfs/dn1"
-/usr/sbin/kadmin.local -q "addprinc -randkey HTTP/dn1"
+/usr/sbin/kadmin.local -q "addprinc -randkey hdfs/nn.default.svc.cluster.local"
+/usr/sbin/kadmin.local -q "addprinc -randkey HTTP/nn.default.svc.cluster.local"
+/usr/sbin/kadmin.local -q "addprinc -randkey hdfs/dn1.default.svc.cluster.local"
+/usr/sbin/kadmin.local -q "addprinc -randkey HTTP/dn1.default.svc.cluster.local"
 
-/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab hdfs/nn"
-/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab HTTP/nn"
-/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab hdfs/dn1"
-/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab HTTP/dn1"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab hdfs/nn.default.svc.cluster.local"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab HTTP/nn.default.svc.cluster.local"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab hdfs/dn1.default.svc.cluster.local"
+/usr/sbin/kadmin.local -q "ktadd -k /var/keytabs/hdfs.keytab HTTP/dn1.default.svc.cluster.local"
 
 chown hdfs /var/keytabs/hdfs.keytab
 
-
-keytool -genkey -alias nn -keyalg rsa -keysize 1024 -dname "CN=nn" -keypass changeme -keystore /var/keytabs/hdfs.jks -storepass changeme
-keytool -genkey -alias dn1 -keyalg rsa -keysize 1024 -dname "CN=dn1" -keypass changeme -keystore /var/keytabs/hdfs.jks -storepass changeme
+keytool -genkey -alias nn.default.svc.cluster.local -keyalg rsa -keysize 1024 -dname "CN=nn.default.svc.cluster.local" -keypass changeme -keystore /var/keytabs/hdfs.jks -storepass changeme
+keytool -genkey -alias dn1.default.svc.cluster.local -keyalg rsa -keysize 1024 -dname "CN=dn1.default.svc.cluster.local" -keypass changeme -keystore /var/keytabs/hdfs.jks -storepass changeme
 
 chmod 700 /var/keytabs/hdfs.jks
 chown hdfs /var/keytabs/hdfs.jks
